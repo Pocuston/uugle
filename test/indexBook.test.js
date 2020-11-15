7434283cbf9cfbf4c58d79f863833f4627df0e73
Jakub Šlambora <jakub.slambora@unicorn.com>
2020-11-15 17:29:56 +0100

    Test more books can be indexed

--- test/indexBook.test.js
+++ test/indexBook.test.js
@@ -1,6 +1,11 @@
 import FDBFactory from "fake-indexeddb/lib/FDBFactory";
 import { indexBook } from "../src/pages/Background/storage";
-import { uu5Book, uu5BookData } from "./testData";
+import {
+  uu5Book,
+  uu5BookData,
+  uuAppFrameworkBook,
+  uuAppFrameworkBookData,
+} from "./testData";
 import { getBooks, getTransaction } from "./testUtils";
 
 beforeEach(() => {
@@ -8,22 +13,28 @@ beforeEach(() => {
 });
 
 test("Book is put into book object store", async () => {
-  const bookData = uu5BookData;
-
-  await indexBook(bookData);
+  await indexBook(uu5BookData);
 
   const books = await getBooks();
   expect(books).toContainEqual(uu5Book);
   expect(books).toHaveLength(1);
 });
 
-test("Same book is indexed only once", async () => {
-  const bookData = uu5BookData;
-
-  await indexBook(bookData);
-  await indexBook(bookData);
+test("Same book is put into book object store only once", async () => {
+  await indexBook(uu5BookData);
+  await indexBook(uu5BookData);
 
   const books = await getBooks();
   expect(books).toContainEqual(uu5Book);
   expect(books).toHaveLength(1);
+});
+
+test("More books can be put into book object store", async () => {
+  await indexBook(uu5BookData);
+  await indexBook(uuAppFrameworkBookData);
+
+  const books = await getBooks();
+  expect(books).toContainEqual(uu5Book);
+  expect(books).toContainEqual(uuAppFrameworkBook);
+  expect(books).toHaveLength(2);
 });
--- test/testData.js
+++ test/testData.js
@@ -17,3 +17,23 @@ export const uu5Book = {
   url:
     "https://uuos9.plus4u.net/uu-bookkitg01-main/78462435-ed11ec379073476db0aa295ad6c00178",
 };
+
+export const uuAppFrameworkBookData = {
+  url:
+    "https://uuapp.plus4u.net/uu-bookkit-maing01/25d3b166760a44b7be70e5c2eb2abaaa/book/page?code=Insert25d3b166760a47b",
+  loadBook: {
+    primaryLanguage: "en",
+    name: {
+      en: "uuApp Framework",
+    },
+  },
+  getBookStructure: {
+    itemMap: {},
+  },
+};
+
+export const uuAppFrameworkBook = {
+  name: "uuApp Framework",
+  url:
+    "https://uuapp.plus4u.net/uu-bookkit-maing01/25d3b166760a44b7be70e5c2eb2abaaa",
+};
