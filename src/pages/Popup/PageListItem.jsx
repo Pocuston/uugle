f311e23a6dfc1b96d2db617ac382c5ffc87a6f7f
Jakub Šlambora <jakub.slambora@unicorn.com>
2021-11-07 16:43:55 +0100

    Tweak breadcrumbs UI

--- src/pages/Popup/PageListItem.jsx
+++ src/pages/Popup/PageListItem.jsx
@@ -1,16 +1,12 @@
 import { Link, ListItem, ListItemText, Typography } from "@material-ui/core";
 import React, { useLayoutEffect, useRef } from "react";
 import { makeStyles } from "@material-ui/core/styles";
+import KeyboardArrowRightIcon from "@material-ui/icons/ArrowRight";
 import PageState from "./PageState";
 
 const useStyles = makeStyles(theme => ({
-  listIconItem: {
-    marginLeft: "4px",
-    minWidth: "26px",
-  },
-  pageIcon: {
-    color: theme.palette.grey.A700,
-    fontSize: "1.2rem",
+  listItem: {
+    paddingLeft: "8px",
   },
   pageLink: ({ color }) => ({
     fontWeight: "500",
@@ -19,9 +15,6 @@ const useStyles = makeStyles(theme => ({
   breadcrumbLink: ({ color }) => ({
     color: color,
   }),
-  listItemIconImage: {
-    opacity: 0.8,
-  },
 }));
 
 export default function PageListItem({ page, selected, onLinkClick }) {
@@ -44,16 +37,23 @@ export default function PageListItem({ page, selected, onLinkClick }) {
     event.stopPropagation();
   }
 
+  function showBreadCrumbs() {
+    return page.breadcrumbs && page.breadcrumbs.length > 0;
+  }
+
+  function showHomeLink() {
+    return page.bookName && page.bookUrl && showBreadCrumbs();
+  }
+
   return (
     <ListItem
-      key={`${page.awid}/${page.code}`}
       button
       disableGutters
       selected={selected}
       ref={selected ? selectedLisItemRef : null}
       alignItems={"flex-start"}
       onClick={event => handleLinkClick(event, page.url)}
-      style={{ paddingLeft: "16px" }}
+      className={classes.listItem}
     >
       <ListItemText
         primary={
@@ -72,35 +72,37 @@ export default function PageListItem({ page, selected, onLinkClick }) {
         }
         secondary={
           <Typography variant={"caption"}>
-            {page.bookName && page.bookUrl && (
+            {showHomeLink() && (
               <Link
                 title={page.bookUrl}
                 href={page.bookUrl}
                 onClick={event => handleLinkClick(event, page.bookUrl)}
                 className={classes.breadcrumbLink}
               >
-                {page.bookName}
+                Home
               </Link>
-            )}{" "}
-            {page.breadcrumbs &&
+            )}
+            {showBreadCrumbs() &&
               page.breadcrumbs.map(breadcrumb => {
                 const breadcrumbUrl = getBreadcrumbUrl(
                   page.bookUrl,
                   breadcrumb.code
                 );
                 return (
-                  <>
-                    >{" "}
+                  <React.Fragment key={breadcrumb.code}>
+                    <KeyboardArrowRightIcon
+                      style={{ verticalAlign: "text-bottom", fontSize: "16px" }}
+                    />
                     <Link
+                      key={breadcrumb.code}
                       title={breadcrumbUrl}
                       href={breadcrumbUrl}
                       onClick={event => handleLinkClick(event, breadcrumbUrl)}
                       className={classes.breadcrumbLink}
                     >
-                      {" "}
-                      {breadcrumb.name}{" "}
+                      {breadcrumb.name}
                     </Link>
-                  </>
+                  </React.Fragment>
                 );
               })}
           </Typography>
